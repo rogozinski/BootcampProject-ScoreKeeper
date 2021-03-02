91fcd8bb67204bc2dde0aca4bb69eec88940e265
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-02 15:45:40 +0100

    ScoreKeeper update
    if one of the players reach winning score then function won't allow to add more points

--- app.js
+++ app.js
@@ -11,19 +11,26 @@ let p2Score = 0;
 //Reset Scores
 const resetButton = document.querySelector("#resetButton");
 
-//we can keep adding points until it reach winningScore value
+//we can keep adding points until it one of the player score reach winningScore. After than isGameOver is set to true which disabling possibility to add more points
 let winningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener("click", function () {
-    if (p1Score !== winningScore) {
+    if (!isGameOver) {
         p1Score += 1;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+        }
         p1Display.textContent = p1Score;
     }
 });
 
 p2Button.addEventListener("click", function () {
-    if (p2Score !== winningScore) {
+    if (!isGameOver) {
         p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+        }
         p2Display.textContent = p2Score;
     }
 });
